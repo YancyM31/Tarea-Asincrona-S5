024924f1faabf73f67419182739c2b4e17d03141
YancyM31 <126370497+YancyM31@users.noreply.github.com>
2023-02-25 21:58:56 -0600

    Add files via upload

---
+++ Actividad-Asíncrona S5.py
@@ -0,0 +1,116 @@
+print ("Hola Ingeniero somos estudiantes de la clase de programación")
+print ("Nombres de integrantes")
+print ("")
+a= "INTEGRANTE 1"
+print (a)
+nombreUno= "Alicia"
+nombreDos= "Estefany"
+apellidoUno= "Guzmán"
+apellidoDos= "López"
+print (nombreUno,nombreDos,apellidoUno,apellidoDos)
+g= "Sexo : Femenino"
+print (g)
+e= "Edad : 17"
+print (e)
+edadUno= 17
+egresar= 5
+edadEgresar=edadUno + egresar
+print ("La edad de Alicia al egresar de la carrera eserá de : ",edadEgresar),print ("años")
+
+       
+
+print ("")
+print ("ESTO ES UNA ASIGNACIÓN DE CADENAS DE CARACTERES")
+mensaje= "Hola soy Alicia"
+buscarSubCadena= mensaje.find("Alicia")
+print (buscarSubCadena)
+print ("")
+
+
+b= "INTEGRANTE 2"
+print (b)
+nameOne= "Ana"
+nameTwo= "Yancy"
+lastNameOne= "Azúcar"
+lastNameTwo= "Mejía"
+print (nameOne,nameTwo,lastNameOne,lastNameTwo)
+G= "Sexo : Femenino"
+print (G)
+age= "Edad : 18"
+print (age)
+edadDos= 18
+egresar= 5
+edadEgresar= edadDos + egresar
+print ("La edad de Ana al egresar de la carrera será de: ",edadEgresar),print ("años")
+print ("")
+
+
+c= "INTEGRANTE 3"
+print (c)
+nombres= "Jonathan Alexander"
+apellidos= "Tejada Jiménez"
+print (nombres,apellidos)
+s= "Sexo : Masculino"
+print (s)
+ed= "Edad : 20"
+print (ed)
+edadTres= 20
+egresar= 5
+edadEgresar= edadTres + egresar
+print ("La edad de Jonathan al egresar de la carrera será de: ",edadEgresar),print ("años")
+print ("")
+
+
+f= "INTEGRANTE 4"
+print (f)
+nombre= "Romilio David"
+apellido= "Galdámez Escobar"
+print (nombre,apellido)
+se= "Sexo : Masculino"
+print (se)
+eda= "Edad : 17"
+print (eda)
+edadCuatro= 17
+egresar= 5
+edadEgresar= edadCuatro + egresar
+print ("La edad de Romilio al egresar de la carrera será de: ",edadEgresar),print ("años")
+print ("")
+
+
+d= "INTEGRANTE 5"
+print (d)
+names= "Mauricio Orlando"
+lastnames= "Vaquero Alas"
+print (names,lastnames)
+se= "Sexo : Masculino"
+print (se)
+ag= "Edad : 22"
+print (ag)
+edadCinco= 22
+egresar= 5
+edadEgresar= edadCinco + egresar
+print ("La edad de Mauricio al egresar de la carrera será de: ",edadEgresar),print ("años")
+print ("")
+
+
+print ("El promedio de edad de los integrantes del grupo es : ")
+edadUno= 17
+edadDos= 18
+edadTres= 20
+edadCuatro= 17
+edadCinco= 22
+dividendo= 5
+resultado= edadUno + edadDos + edadTres + edadCuatro + edadCinco
+print ("Suma de edades : ", resultado)
+print ("Promedio de edades : ", resultado / dividendo)
+
+
+
+
+
+
+
+
+
+
+
